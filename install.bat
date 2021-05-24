732848c79d05255041ae0a5f4a48eff5f88b4e79
veryliumeng <veryliumeng@qq.com>
2021-05-24 09:18:46 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,13 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 @echo off
+powershell unblock-file -path easywork.ps1
 :: Change HKCU to HKLM if you want to install globally.
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\smartforce" /ve /t REG_SZ /d "%~dp0easywork.json" /f
+echo.
+echo.
+echo ==== please restart browser to use easywork ====
+echo.
+echo.
 PAUSE
